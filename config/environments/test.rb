5d238016bcc026be60df44b29783f17a48c0c75b
LMafra <chagas.lucas.mafra@gmail.com>
2025-09-07 17:34:40 -0300

    test(sidekiq): fix sidekiq test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -63,4 +63,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Use inline job adapter for tests to avoid Redis dependency
+  config.active_job.queue_adapter = :test
+
+  # Configure Sidekiq to use fake mode for tests (no Redis required)
+  require 'sidekiq/testing'
+  Sidekiq::Testing.fake!
 end
